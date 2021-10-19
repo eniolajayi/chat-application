1bca14d4cacaa054b4885f2678a921a5b565d484
Eniola Ajayi <ajayieniola16@gmail.com>
2021-10-19 15:08:57 +0100

    create and configure server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "public")));
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
