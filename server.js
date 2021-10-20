03c8e30986584a5753d1eb26997c92b9db16cfdf
Eniola Ajayi <ajayieniola16@gmail.com>
2021-10-20 15:27:42 +0100

    listen for socket connection

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const PORT = 3000 || process.env.PORT;
+const botName = "MEE6";
 
 app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("connected to socket");
+});
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
