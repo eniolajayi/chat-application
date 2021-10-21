88e802cd686d13756fabd5610c3d63fa0dd7990c
Eniola Ajayi <ajayieniola16@gmail.com>
2021-10-21 01:14:13 +0100

    create logic for handling client events

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const createMessageData = require("./utils/formatting");
+const {
+  addUser,
+  removeUser,
+  getCurrentUser,
+  getRoomUsers,
+  getAllUsers,
+} = require("./utils/user");
+
+// const MESSAGE_EVENT ="message";
+// const MESSAGE_IN_CHAT_EVENT ="";
+// const JOIN_ROOM_EVENT ="";
+// const USERS_IN_ROOM_EVENT ="";
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +26,53 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   console.log("connected to socket");
+
+  // On user joining room
+  socket.on("join_room", ({ username, room }) => {
+    const user = addUser(socket.id, username, room);
+    socket.join(user.room);
+    // Welcome user
+    socket.emit("message", () =>
+      createMessageData(botName, "Welcome, Let's get chatty")
+    );
+    // let room know whena user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        createMessageData(botName, `${user.username} as joined the chat`)
+      );
+    // send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // listen for chat message
+  socket.on("chat_message", (msg) => {
+    const user = getCurrentUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", createMessageData(user.username, msg));
+    }
+  });
+
+  // send event when user disconnects
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        createMessageData(botName, `${user.username} as left the chat`)
+      );
+
+      // send info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
 });
 
 server.listen(PORT, () => {
