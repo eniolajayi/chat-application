2c98aa1e5a2d72146eb0655c69214a071825046b
Eniola Ajayi <ajayieniola16@gmail.com>
2021-10-22 13:37:10 +0100

    create client side logic

---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+const ready = function (callback) {
+  document.readyState === "loading"
+    ? document.addEventListener("DOMContentLoaded", function (e) {
+        callback();
+      })
+    : callback();
+};
+
+// Usage
+ready(function () {
+  // // get required elements from DOM
+  const chatForm = document.getElementById("chat-form");
+  const chatMessages = document.querySelector(".chat-messages");
+  const roomName = document.getElementById("room-name");
+  const userList = document.getElementById("users");
+  const socket = io();
+  const title = document.title;
+  // // get username and room from url
+  const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+
+  document.title = `${title} - ${username}`;
+  // join chat room
+  socket.emit("join_room", { username, room });
+
+  // get room users
+  socket.on("room_users", ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+  });
+
+  // get message from server
+  socket.on("message", (message) => {
+    outputMessage(message);
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  });
+
+  // send message
+  chatForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = e.target.elements.msg.value;
+    console.log(message);
+    // emit message to server
+    socket.emit("chat_message", message);
+
+    //clear input
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
+  });
+
+  function outputMessage(message) {
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.innerHTML = `
+    <p class="meta"> ${message.username} <span class="time">${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>
+    `;
+    document.querySelector(".chat-messages").appendChild(div);
+  }
+
+  // add room name to DOM
+  function outputRoomName(room) {
+    roomName.innerText = room;
+  }
+
+  // Add users to dom
+  function outputUsers(users) {
+    userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join("")}
+    `;
+  }
+});
